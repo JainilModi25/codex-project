812d6d6f699bd58c2e7c39802f70b42e1225e5c3
JainilModi <jainilrmodi2013@gmail.com>
2023-04-13 10:34:19 +0530

    update the server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   // messageDiv.innerHTML = "..."
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000", {
+  const response = await fetch("https://codex-e2yi.onrender.com/", {
     // mode: "no-cors",
     method: "POST",
     headers: {
